511bbaeb79dc410de2b60dccc4003a52103f9037
Jaromil <jaromil@dyne.org>
2014-11-24 13:07:07 +0100

    Initial import from VUA

---
+++ config.zsh
@@ -0,0 +1,28 @@
+TITLE="Roll up your sleeves, we may need to fork Debian."
+DESCRIPTION="A website made by Veteran Unix Admins to organize and protest against the decision to impose SystemD to Debian users."
+KEYWORDS="debian, systemv, sysv, systemd, gnu, linux, distro, politics"
+
+#
+# Anything below is safe to leave untouched
+#
+
+# What file extension to use for html files
+EXTENSION=".html"
+
+# What is the root of this website url, after the domain
+# leave blank if relative, or for instance /blog/
+WEB_ROOT=""
+
+# What is the url for files in case indexing is used
+# this can be different from WEB_ROOT in order to serve
+# files from a position different from the web pages
+# FILES_ROOT=""
+ 
+# Uncomment for Flowtype
+FLOWTYPE=1
+# What is the size ratio of text with respect to the width
+# of its container element. This is better than setting the
+# text size in an absolute way (we use Flowtype)
+FONT_RATIO=30
+
+
